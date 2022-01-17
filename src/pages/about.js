6af58a73e3cffc393b3b5490db7cade420c9e234
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-01-17 20:26:35 +0900

    feat: add simple page and about page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>Learn more</title>
+      <h1>What is Pipu</h1>
+      <ol>
+        <li>One match at a time</li>
+        <li>Profile unblurs the more you get to know the other person</li>
+        <li>Learn about a persons, Goals, Values, and interests</li>
+      </ol>
+    </main>
+  );
+};
+
+export default AboutPage;
