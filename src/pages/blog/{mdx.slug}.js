a61d6ad3e840e18a71e0e49bf0bd9ad1d1e8c566
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-01-20 14:02:25 +0900

    feat: add her image and gatsby-sharp-plugin

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,10 +1,13 @@
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import * as React from "react";
 import Layout from "../layout";
-import { title, posted, body } from "./index.module.css";
+import { title, posted, body, credit } from "./index.module.css";
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle="Stories">
       <p>Read people's experience on using pipu</p>
@@ -13,6 +16,16 @@ const BlogPost = ({ data }) => {
         <article key={data.mdx.id}>
           <h2 className={title}>{data.mdx.frontmatter.title}</h2>
           <p className={posted}>Posted: {data.mdx.frontmatter.date}</p>
+          <GatsbyImage
+            image={image}
+            alt={data.mdx.frontmatter.hero_image_alt}
+          />
+          <p>
+            <span className={credit}>Photo Credit:</span>
+            <a href={data.mdx.frontmatter.hero_image_credit_link}>
+              {data.mdx.frontmatter.hero_image_credit_text}
+            </a>
+          </p>
           <div className={body}>
             <MDXRenderer>{data.mdx.body}</MDXRenderer>
           </div>
@@ -25,11 +38,19 @@ const BlogPost = ({ data }) => {
 export const query = graphql`
   query MyQuery($id: String) {
     mdx(id: { eq: $id }) {
+      body
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `;
